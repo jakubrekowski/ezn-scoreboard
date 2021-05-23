0081d0ca9bb5d663e27cb1c3cfdbdbcc686d7a93
Jakub Rekowski <jakub.rekowski@gmail.com>
2021-05-23 19:09:28 +0200

    init

---
+++ src/dashboard.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    pointsA: '003',
+    pointsB: '042',
+    setsA: '2',
+    setsB: '1',
+    faulsA: '3',
+    faulsB: '4',
+    time: '12:34'
+  }
+})
