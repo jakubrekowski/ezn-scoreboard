6c6538d9e64930f7f2f79351c6fe0339b4eeb0a4
Jakub Rekowski <jakub.rekowski@gmail.com>
2021-06-21 06:38:20 +0200

    move connection from index to dashboard

--- src/connect.js
+++ src/connect.js
@@ -4,12 +4,25 @@ const app = new Vue({
 
   },
   methods: {
-    connectBlue() {
-      navigator.bluetooth.requestDevice({
-        filters: [{ services: ['battery_service'] }]
+    async connectBlue() {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{ name: 'EZN Scoreboard' }],
+        optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b']
       })
-        .then(device => console.log(device))
-        .catch(err => console.error(err))
+      
+      const server = await device.gatt.connect()
+      const service = await server.getPrimaryService('4fafc201-1fb5-459e-8fcc-c5c9c331914b')
+      const characteristic = await service.getCharacteristic('beb5483e-36e1-4688-b7f5-ea07361b26a8')
+
+      const textEncoder = new TextEncoder('utf-8')
+      const textDecoder = new TextDecoder('utf-8')
+
+      const value = await characteristic.readValue()
+      characteristic.writeValue(textEncoder.encode('papua nowa gwinea'))
+
+      console.log(textDecoder.decode(value))
+
+      console.log(server, service, characteristic)
     }
   }
 })
--- src/dashboard.js
+++ src/dashboard.js
@@ -10,6 +10,7 @@ const app = new Vue({
     time: '00:00',
     timeSet: '10',
     //  ui controls
+    bleConnected: false,
     showProfileSelect: false,
     selectedProfile: 0,
     profile: [
@@ -37,10 +38,49 @@ const app = new Vue({
       } 
       this.timePause = false
       this.timeStop = false
+    },
+    async connectBlue() {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{ name: 'EZN Scoreboard' }],
+        optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b']
+      })
+
+      this.bleConnected = true
+      
+      server = await device.gatt.connect()
+      
+      if (!server.connected) {
+        return
+      } 
+      
+      service = await server.getPrimaryService('4fafc201-1fb5-459e-8fcc-c5c9c331914b')
+
+      characteristics.lpoint = await service.getCharacteristic('0a355012-692a-4722-a6a0-1370461b4b24')
+      characteristics.ladd1 = await service.getCharacteristic('c44dd96e-4de9-45b6-9c43-d23ec9bcacbc')
+      characteristics.ladd2 = await service.getCharacteristic('94fcc81f-8205-4a99-bf7a-22d10ae33d90')
+      characteristics.rpoint = await service.getCharacteristic('c0a45579-03fb-4887-8609-f3d05a83c667')
+      characteristics.radd1 = await service.getCharacteristic('d4ff2e42-9d3d-41f7-8b52-0e1b8dd49ac2')
+      characteristics.radd2 = await service.getCharacteristic('ce5d94b6-9666-4404-83a6-4bd9b88b2dd2')
+      characteristics.tswich = await service.getCharacteristic('3f7cb528-820e-45e9-9b5e-c359fde40c1e')
+      // characteristics.tvalue = await service.getCharacteristic('9dfde2a1-906a-4a4f-88d6-f361b719933d')
+
+      // const characteristic = await service.getCharacteristic('beb5483e-36e1-4688-b7f5-ea07361b26a8')
+
+      // const textEncoder = new TextEncoder('utf-8')
+      // const textDecoder = new TextDecoder('utf-8')
+
+      // const value = await characteristic.readValue()
+      // characteristic.writeValue(textEncoder.encode('papua nowa gwinea'))
+
+      // console.log(textDecoder.decode(value))
     }
   }
 })
 
+// ble stack
+let server, service, characteristics = {}
+
+// time stack
 setInterval(() => updateCountdown(), 1000)
 
 let timeDown 
